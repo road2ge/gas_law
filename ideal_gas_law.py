e51d16c64f252e08e5457269d49c5f669c480eec
kmiller <kalemiller@simplecyberspace.com>
2017-05-16 13:53:01 -0400

    added static condition
    If conditions don't change it's an entirely different set of rules. Added.

--- ideal_gas_law.py
+++ ideal_gas_law.py
@@ -1,15 +1,4 @@
 ########Gas Laws (Honors Chemistry)########
-constant = raw_input("Is your constant T, V, or P? ").lower()
-temp_unit = "k"
-if not constant == 't':
-    temp_unit = raw_input("Are your temps given in C, F or K? ")
-if not temp_unit.lower() in "cfk":
-    temp_unit = raw_input("You typed something wrongly. What unit? ")
-
-print "Input 'u' for unknown.\nSince we have two data sets, put your one unknown in the second data set."
-print "Input 'c' for constant."
-args1 = raw_input("Temperature,Volume,Pressure where volume is in L and pressure is in atm> ").split(',')
-args2 = raw_input("Second set of data. Just like before> ").split(',')
 
 def which_law(const):
     if const == "t":
@@ -32,28 +21,14 @@ def convert_f_k(temperature):
     except:
         return temperature
 
-# Process temperature into Kelvin
-if not constant == 't':    
-    if not temp_unit.lower() == "k":
-        if temp_unit.lower() == "f":
-            args1[0] = convert_f_k(args1[0])
-            args2[0] = convert_f_k(args2[0])
-        elif temp_unit.lower() == "c":
-            args1[0] = convert_c_k(args1[0])
-            args2[0] = convert_c_k(args2[0])
-
-
-
-
 # Gas law is PV = nRT. n is constant.
 def boyles(a1,b1,a2):
-    # Temp is constant. Find b2.
+    # temperature is constant. Find b2.
     # a1 * b1 = a2 * b2
     prod = a1 * b1
     b2 = prod / a2
     # TODO: PROCESS A RET STRING
     print b2
-
 def gay_lussacs(a1,b1,a2,t_p=False):
     # Volume is constant. Find b2.
     # p/t = p/t
@@ -73,8 +48,81 @@ def charles(a1,b1,a2,t_v=False):
     else:
         b2 = a1 * a2 / b1
         print str(b2) + " liters is your new volume"
+cond_change = raw_input("Do conditions change? y/n>")
+if cond_change == 'y':
+    cond_change = True
+elif cond_change == 'n':
+    cond_change = False
+else:
+    while not cond_change in 'yn':
+        cond_change = raw_input("I don't understand.\nDo conditions change? y/n>")
+if not cond_change:
+    constant = raw_input("Is your constant T, V, or P>").lower()
+    temperature_unit = "k"
+    if not constant == 't':
+        temperature_unit = raw_input("Are your temperatures given in C, F or K>")
+    if not temperature_unit.lower() in "cfk":
+        temperature_unit = raw_input("You typed something wrongly. What unit>")
 
+    print "Input 'u' for unknown.\nSince we have two data sets, put your one unknown in the second data set."
+    print "Input 'c' for constant."
+    args1 = input("Temperature,Volume,Pressure where volume is in L and pressure is in atm>").split(',')
+    args2 = input("Second set of data. Just like before>").split(',')
 
+# Process temperature into Kelvin
+if not constant == 't':    
+    if not temperature_unit.lower() == "k":
+        if temperature_unit.lower() == "f":
+            args1[0] = convert_f_k(args1[0])
+            args2[0] = convert_f_k(args2[0])
+        elif temperature_unit.lower() == "c":
+            args1[0] = convert_c_k(args1[0])
+            args2[0] = convert_c_k(args2[0])
+
+else:
+    r = .082
+    unknown = raw_input("Is your unknown P, V, Mass (M), or T>").lower()
+    while unknown not in 'pvmt':
+        unknown = raw_input("I don't understand.\nIs your unknown P, V, Mass (M), or T>").lower()
+    if not unknown == 'p':
+        pressure = input("Pressure in atm>")
+    if not unknown == 'v':
+        volume = input("Volume in liters>")
+    if not unknown == 'm'
+        mass_unit = raw_input("If you have moles, input y. If you have grams, input n>").lower()
+        if mass_unit == 'y':
+            mass_unit = True
+        elif mass_unit == 'n':
+            mass_unit = False
+            mass = 'How many grams do you have then>'
+        else:
+            while not mass_unit in 'ynu':
+                mass_unit = raw_input("I don't understand.\nIf you have moles, input y. If you have grams, input n>").lower()
+        if not mass_unit:
+            mm = input("What is the molar mass of this gas>")
+            moles = mass / mm
+    if not unknown == 't':
+        temperature_unit = raw_input("Temperature unit: C, F or K>").lower()
+        if not temperature_unit in 'cfk':
+            while temperature_unit not in 'cfk':
+                temperature_unit = raw_input("I don't understand.\nTemperature unit: C, F, or K>").lower()
+        temperature = input("Now actual number for temperature>")
+        if not temperatureerature_unit == 'k':
+            if temperatureerature_unit == 'f':
+                temperature = convert_f_k(temperature)
+            else:
+                temperature = convert_c_k(temperature)
+    if unknown == 'p':
+        print str(mass * r * temperature / volume) + " atm."
+    elif unknown == 'v':
+        print str(mass * r * temperature / pressure) + " Liters."
+    elif unknown == 'm':
+        print str(pressure * volume / (r*temperature)) + " moles, or alternatively"
+        print str(mm * moles) + " grams."
+    elif unknown == 't':
+        print str(pressure * volume / (r*mass)) + " Celsius."
+
+        
 # Given T,V,P,T2,V2,P2
 law = which_law(constant)
 if law == 'b':
